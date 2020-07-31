1f06127b7e095298a8e21fb470cbef9fa3aaf061
Iryna Loza <irynloza@gmail.com>
2020-07-31 13:58:51 -0700

    Update

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,9 +3,16 @@
 function Homepage() {
   return (
     <div>
-      This component needs to be implemented.
+      <h4>Hello dear user!</h4>
+      <a href="/cards">Click here to go to the cards</a>
+      <br></br>
+      <br></br>
+      <img src="/static/img/balloonicorn.jpg"></img>
     </div>
   );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
+
+
+
